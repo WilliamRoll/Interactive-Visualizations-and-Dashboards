afb03371e8c63717ea118ae54684e6317c8f5b76
WilliamRoll <broll6595@gmail.com>
2021-01-08 21:36:53 -0500

    starting to work on dropdown

--- Instructions/StarterCode/static/js/app_2.js
+++ Instructions/StarterCode/static/js/app_2.js
@@ -1,9 +1,22 @@
 //Populate Dropdown Menu with Sample ID's
 function init(){
+    
+    //D3 to select the dropdown menu
     var dropDown = d3.select("#selDataset")
 
-//Reads the data from the json file
-d3.json("samples.json").then((importedData) => {
-     console.log(importedData)   
-});
+    //Reads the data from the json file
+    d3.json("samples.json").then((data) => {
+        
+        //assign a variable to data
+        var importedData = data
+        console.log(importedData)
+
+        //use names to populate dropdown using for loop
+        importedData.names.forEach(function(n){
+            dropdownMenu.property("value")
+        })
+        
+    });
 }
+
+init();
